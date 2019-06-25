9b65d42db5c46ff59adc17d2cee82046bfdec120
baiyongcheng <18215637262@163.com>
2019-06-25 10:24:33 +0800

    添加filter

--- api-service/src/main/java/com/byc/ApiServiceApplication.java
+++ api-service/src/main/java/com/byc/ApiServiceApplication.java
@@ -1,7 +1,11 @@
 package com.byc;
 
+import com.byc.filter.MyFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiServiceApplication {
@@ -10,4 +14,16 @@ public class ApiServiceApplication {
 		SpringApplication.run(ApiServiceApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator customerRouteLocator(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/customer/**")
+						.filters(f -> f.filter(new MyFilter())
+						.addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+						.uri("http://47.105.192.33:8080")
+						.order(0)
+						.id("customer_filter_router")
+				)
+				.build();
+	}
 }
