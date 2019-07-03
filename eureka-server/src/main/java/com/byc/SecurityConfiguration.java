0ae92a991d9e76e9493b93fceb5fbf6b54a0432a
baiyongcheng <18215637262@163.com>
2019-07-03 16:27:30 +0800

    添加nacos

---
+++ eureka-server/src/main/java/com/byc/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.byc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by baiyc
+ * 2019/7/3/003 10:48
+ * Description：开启授权访问
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
